7a5021bdb88409aaef3170b83bb8f1ac1e2875b8
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-07-16 16:56:02 +0200

    vehicles add logic, assets, style

---
+++ src/js/vehicles.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function(event){
+	if(document.querySelector(".rendering_container")){
+  
+	  const renderingContainers = document.querySelectorAll(".rendering_container");
+  
+	  const fetchVehicles = async () => {
+		try {
+		  const response = await fetch("https://swapi.dev/api/vehicles/");
+		  const result = await response.json();
+		  console.log(result);
+		  return result.results; // Return the array of vehicles
+		} catch (error) { 
+		  console.log(("An error occurred: " + error.message));
+		}
+	  };
+  
+	  async function renderVehicles() {
+		const vehicles = await fetchVehicles(); // Call fetchVehicles to get the vehicle data
+		vehicles.splice(0,7).forEach((vehicle, index) => {
+		  try {
+			if (vehicle.name === "TIE/LN starfighter") { //Skip rendering for TIE/LN starfighter
+				return;
+			  }
+			// Create elements
+			const vehicleContainer = document.createElement("article");
+			vehicleContainer.className = "vehicle";
+			const infoContainer = document.createElement("ul");
+			infoContainer.className = "vehicle-list";
+			const posterContainer = document.createElement("img");
+  
+			// Append data to created elements
+			vehicleContainer.append(infoContainer, posterContainer);
+			vehicleContainer.append(posterContainer);
+  
+			// Append vehicle container to the appropriate rendering container
+			renderingContainers[index % renderingContainers.length].appendChild(
+			  vehicleContainer
+			);
+  
+			// Render elements with data fetched from API
+			infoContainer.innerHTML = `<li>Name: ${vehicle.name}</li><li>Model: ${vehicle.model}</li><li>Class: ${vehicle.vehicle_class}</li><li>Crew: ${vehicle.crew} people</li><li>Manufactured by: ${vehicle.manufacturer}</li>`;
+			const imageName = vehicle.name.split(" ").join("-");
+			posterContainer.src = `../assets/vehicles/${imageName}.jpg`;
+			posterContainer.alt = "Vehicle image";
+		  } catch (error) {
+			console.log(("An error occurred: " + error.message));
+		  }
+		});
+	  }
+  
+	  renderVehicles();
+  
+	}
+  });
+  
